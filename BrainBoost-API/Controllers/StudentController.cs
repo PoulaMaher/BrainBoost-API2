19692daa0f891c29632bc14e04a7ee6fbb3f8178
marian <marian.rashed23@gmail.com>
2024-06-19 15:48:33 +0300

    Made TopStudent And Enrolled Courses Endpoint

--- BrainBoost-API/Controllers/StudentController.cs
+++ BrainBoost-API/Controllers/StudentController.cs
@@ -95,5 +95,6 @@ namespace BrainBoost_API.Controllers
             int numofenrolledcourse = UnitOfWork.StudentRepository.GetTotalNumOfEnrolledCourses();
             return Ok(numofenrolledcourse);
         }
+
     }
 }
