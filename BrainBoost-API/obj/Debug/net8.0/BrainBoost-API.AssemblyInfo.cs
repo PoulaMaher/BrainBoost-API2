109dd5cbc59314abdf5b7640eabd8d0491aca2dd
PoulaMaher <poula.maher199@gmail.com>
2024-06-29 19:30:53 +0300

    poula

--- BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
+++ BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676173d35cb9cbaef7916dcdbd1dc0a3bd737c4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf25872f28ea9b339098335ff142bf30913b4484")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
