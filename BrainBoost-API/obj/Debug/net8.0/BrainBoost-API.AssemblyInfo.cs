28f357a8e9efe5998758ecef0a132d3495b3759b
PoulaMaher <poula.maher199@gmail.com>
2024-06-30 18:23:50 +0300

    Change CS

--- BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
+++ BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109dd5cbc59314abdf5b7640eabd8d0491aca2dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0a5ad4689b57be1ce32f133211ba29afa56db4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
