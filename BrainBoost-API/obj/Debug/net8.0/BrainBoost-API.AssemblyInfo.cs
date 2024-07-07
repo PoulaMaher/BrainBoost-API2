2395f19a18b0b37346120e7df90bfac6de23ad7f
hagar <hagaribra04@gmail.com>
2024-07-07 17:27:11 +0300

    anything

--- BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
+++ BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f8cbebcd0db837d6cf18c9ced483e95e861bb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb97088c4cf54b5006caa15627897bc58489521")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
