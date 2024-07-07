770cdbcff89210163846ac428cd23d1ae3157df2
tomas wagdy <tomaswagdy549>
2024-07-07 07:04:56 +0300

    a

--- BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
+++ BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e45ffb6ee7993ff56aab31653624baf2e2675e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a3f3cee67d705dc1b39f579cb226dd74a5da74")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
