ed8f80927a6cb9601e8653ae0034e15b4dff98a3
tomas wagdy <tomaswagdy549>
2024-07-08 11:41:28 +0300

    edit videos

--- BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
+++ BrainBoost-API/obj/Debug/net8.0/BrainBoost-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b7b11b8d0a9fdd1590e1ffa7531fc9a187cd29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e7d8be31180038d42a50f351ccb8c99e9b3e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainBoost-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
